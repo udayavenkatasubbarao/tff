ef88289218c71ac566bd9e1293066058e132ee23
udayavenkatasubbarao <91111372+udayavenkatasubbarao@users.noreply.github.com>
2023-09-01 13:33:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,4 @@
-variable "access_key" {
-  default = "AKIAVFBR3CHWJZY2S3UV"
-  }
-variable "secret_key" {
-  default = "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"
-  }
+
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-2"
   }
